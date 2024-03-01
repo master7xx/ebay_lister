800d46d1487dca9332eaa9ecae60786da7f4091d
master7xx <partyzane@gmail.com>
2024-03-01 11:15:30 +0300

    Comments

--- packages/highcharts-container/meteor-postfix.js
+++ packages/highcharts-container/meteor-postfix.js
@@ -1,3 +1,11 @@
+// This line declares a variable named "undefined" and assigns it the value "undefined".
+// However, it is generally not recommended to reassign the value of built-in global variables like "undefined".
 var undefined;
+
+// This line exports the "Highcharts" module and assigns it to the "module.exports" object.
+// This allows the "Highcharts" module to be imported and used in other JavaScript files.
 Highcharts = module.exports;
+
+// This line sets the "module" variable to "undefined".
+// This is not a common practice and can potentially lead to confusion or errors.
 module = undefined;
