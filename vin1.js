246389db6b6567b4450728c9d76cfac304836447
master7xx <partyzane@gmail.com>
2024-03-01 11:16:07 +0300

    Comments

--- vin1.js
+++ vin1.js
@@ -1,25 +1,58 @@
-
-
-
+// Create a new MongoDB collection named "cats"
 Cats = new Mongo.Collection("cats");
+
+// Create a new MongoDB collection named "items"
 Items = new Mongo.Collection("items");
 
-
-Pages2 = new Meteor.Pagination(Items, {templateName: "Tablez", routerLayout:"BrootLay", itemTemplate: "OneItem", perPage: 84, sort: {
+// Initialize a new Meteor pagination instance for the "Items" collection
+// with the following settings:
+// - templateName: "Tablez"
+// - routerLayout: "BrootLay"
+// - itemTemplate: "OneItem"
+// - perPage: 84
+// - sort by inserted date in descending order
+// - available settings for filters and settings
+Pages2 = new Meteor.Pagination(Items, {
+  templateName: "Tablez",
+  routerLayout: "BrootLay",
+  itemTemplate: "OneItem",
+  perPage: 84,
+  sort: {
     inserted: -1
-},
-    availableSettings: {
+  },
+  availableSettings: {
     filters: true,
     settings: true
-}})
+  }
+});
 
-Pages = new Meteor.Pagination(Items, {router: "iron-router", homeRoute: ["/","/photos/"],
-    route: "/photos/", routerTemplate: "Tablez2", itemTemplate: "OneItem",
-    routerLayout:"ZONE", templateName: "Tablez2",
-    perPage: 84, sort: {
+// Initialize a new Meteor pagination instance for the "Items" collection
+// with the following settings:
+// - router: "iron-router"
+// - homeRoute: ["/","/photos/"]
+// - route: "/photos/"
+// - routerTemplate: "Tablez2"
+// - itemTemplate: "OneItem"
+// - routerLayout: "ZONE"
+// - templateName: "Tablez2"
+// - perPage: 84
+// - sort by inserted date in descending order
+// - available settings for filters and settings
+Pages = new Meteor.Pagination(Items, {
+  router: "iron-router",
+  homeRoute: ["/","/photos/"],
+  route: "/photos/",
+  routerTemplate: "Tablez2",
+  itemTemplate: "OneItem",
+  routerLayout: "ZONE",
+  templateName: "Tablez2",
+  perPage: 84,
+  sort: {
     inserted: -1
-},
-    availableSettings: {
-        filters: true,
-        settings: true
-    }})
+  },
+  availableSettings: {
+    filters: true,
+    settings: true
+  }
+});
+
